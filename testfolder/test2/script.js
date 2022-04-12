dc6ba0c6630eecdb9237fbbd498de0ff0dbd770e
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-12 16:47:23 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -1,31 +1,37 @@
-$(document).ready( function () {
-  
-    var table = $('#example').DataTable(); 
-    var table1 = $('#example1').DataTable({
-          "ajax": "https://jeffscoolusername.github.io/testfolder/test2/asatdbackupnolinks.json",
-          "columns": [
-              { "data": "Unit Name" },
-              { "data": "ID" },
-              { "data": "Algo Verified" },
-              { "data": "Name" },
-              { "data": "Date Created" },
-              { "data": "Work History" }
-          ]
-  
+$(document)
+.ready(function() {
+    $.ajax({
+        url: 'asatdbackupnolinks.json',
+        method: 'get',
+        dataType: 'json',
+        success: function(data) {
+            var exampleTable = $('#example')
+                .DataTable({
+                    data: data,
+                    'aaSorting': [[1, 'asc']],
+                    dom: "<'row'<'col-md-6 text-left'T><'col-md-6 text-right'f>>" +
+                        "<'row'<'col-md-12't>>" +
+                        "<'row'<'col-md-6'i><'col-md-6'p>>",
+                    'columnDefs': [
+                        { 'width': '25px', 'targets': [0] },
+                        { 'sortable': false, 'targets': [0] }
+                    ],
+                    'columns': [
+                        { 'data': 'Unit Name' },
+                        { 'data': 'ID' },
+                        { 'data': 'Algo Verified' },
+                        { 'data': 'Name' },
+                        { 'data': 'Date Created' },
+                        { 'data': 'Work History' },
+                        { 'data': 'Algo Grant' },
+                        { 'data': 'Company' },
+                        { 'data': 'Paper' },
+                        { 'data': 'Roadmap' },
+                        { 'data': 'Wallets' },
+                        { 'data': 'Other Links' },
+                    ]
+                });
+        }
     });
-    var table2 = $('#example2').DataTable({
-          ajax: 'https://jeffscoolusername.github.io/testfolder/test2/asatdbackupnolinks.json',
-          "columns": [
-              { "data": "Unit Name" },
-              { "data": "ID" },
-              { "data": "Algo Verified" },
-              { "data": "Name" },
-              { "data": "Date Created" },
-              { "data": "Work History" }
-          ]
-  
-    });
-  
-   
-  } );
-  
+});
+
