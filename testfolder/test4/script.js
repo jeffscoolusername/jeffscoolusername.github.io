73297756826befc2cc7bf8f09b0308a219f0c7af
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-12 21:16:15 -0700

    Create script.js

---
+++ testfolder/test4/script.js
@@ -0,0 +1,32 @@
+$.getJSON( "https://mainnet.analytics.tinyman.org/api/v1/current-asset-prices/", function( data ) {
+   //console.log(data);
+   jsonArr =data;
+   //header
+  if(jsonArr.length >0)
+  {
+    let firstRow =jsonArr[1];
+    let allKeys =Object.keys(firstRow);
+    var tblHeader = document.getElementById("header");
+    allKeys.forEach(function(key) {
+      var newel = document.createElement('th');
+      newel.innerHTML = key;
+      tblHeader.appendChild(newel);
+  });
+
+  var tblBody = document.getElementById("tbody");
+
+  //rows
+  jsonArr.forEach(function(row) {
+     var newTr = document.createElement('tr');
+     allKeys.forEach(function(key) {
+        var newel = document.createElement('td');
+        newel.innerHTML = row[key];
+        newTr.appendChild(newel);
+     });
+    tblBody.appendChild(newTr); 
+  });  
+  }
+
+  $('#example').DataTable();
+
+});
