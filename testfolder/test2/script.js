536c82c77c1c86caef165c183b2557c7a650396b
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-12 20:49:42 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -34,6 +34,16 @@ $(document)
                         { 'data': 'Other Links' },
                     ]
                 });
+
+                $('a.toggle-vis').on( 'click', function (e) {
+                    e.preventDefault();
+             
+                    // Get the column API object
+                    var column = table.column( $(this).attr('data-column') );
+             
+                    // Toggle the visibility
+                    column.visible( ! column.visible() );
+                } );
         }
     });
 });
