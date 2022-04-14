9940d079cb59819d82c986cc694f49f79f202838
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-14 00:55:39 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -15,41 +15,37 @@ $(document)
         {"Unit Name": "AGNR", "ID": "305992851", "Algo Verified": "TBD", "Name": "Algoneer", "Date Created": "Aug 18, 2021", "Work History": "Team (1,2)",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Discord, Ecosystem, GitHub, Reddit, TinyChart"}];
-    $.ajax({
-        method: 'get',
-        dataType: 'json',
-        success: function(data) {
-            var exampleTable = $('#example')
-                .DataTable({
-                    data: jsonObj,
-                    'aaSorting': [[1, 'asc']],
-                    dom: "<'row'<'col-md-6 text-left'T><'col-md-6 text-right'f>>" +
-                        "<'row'<'col-md-12't>>" +
-                        "<'row'<'col-md-6'i><'col-md-6'p>>",
-                    'columnDefs': [
-                        { 'width': '25px', 'targets': [0] }
-                    ],
-                    colReorder: true,
-                    paging: false,
-                    keys: true,
-                    fixedHeader: true,
-                    'columns': [
-                        { 'data': 'Unit Name' },
-                        { 'data': 'ID' },
-                        { 'data': 'Algo Verified' },
-                        { 'data': 'Name' },
-                        { 'data': 'Date Created' },
-                        { 'data': 'Work History' },
-                        { 'data': 'Algo Grant' },
-                        { 'data': 'Company' },
-                        { 'data': 'Paper' },
-                        { 'data': 'Roadmap' },
-                        { 'data': 'Wallets' },
-                        { 'data': 'Other Links' },
-                    ]
-                });
-        }
-    });
+
+    var exampleTable = $('#example')
+        .DataTable({
+            data: jsonObj,
+            'aaSorting': [[1, 'asc']],
+            dom: "<'row'<'col-md-6 text-left'T><'col-md-6 text-right'f>>" +
+                "<'row'<'col-md-12't>>" +
+                "<'row'<'col-md-6'i><'col-md-6'p>>",
+            'columnDefs': [
+                { 'width': '25px', 'targets': [0] }
+            ],
+            colReorder: true,
+            paging: false,
+            keys: true,
+            fixedHeader: true,
+            'columns': [
+                { 'data': 'Unit Name' },
+                { 'data': 'ID' },
+                { 'data': 'Algo Verified' },
+                { 'data': 'Name' },
+                { 'data': 'Date Created' },
+                { 'data': 'Work History' },
+                { 'data': 'Algo Grant' },
+                { 'data': 'Company' },
+                { 'data': 'Paper' },
+                { 'data': 'Roadmap' },
+                { 'data': 'Wallets' },
+                { 'data': 'Other Links' },
+            ]
+        });
+       
 });
 
 
