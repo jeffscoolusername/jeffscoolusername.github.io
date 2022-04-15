326d61af6ac029caad4431da6cd6feb87e564207
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-15 12:23:05 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -14,46 +14,34 @@ $(document)
             }
         };
         xhr.send();
-    };
+    }; 
 
-    var thingie1 = '';   
-    var thingie2 = '';
-    var thingie3 = '';
-    var thingie4 = '';
-    var thingie5 = '';   
-
-    getJSON('https://indexer.algoexplorerapi.io/v2/assets/226265212',
+            getJSON('https://indexer.algoexplorerapi.io/v2/assets/226265212',
           function(err, data) {                  
-            thingie1 = data.asset['asset-tx-counter'];
             getJSON('https://indexer.algoexplorerapi.io/v2/assets/555540865',
           function(err, data2) {    
-            thingie2 = data2.asset['asset-tx-counter'];
             getJSON('https://indexer.algoexplorerapi.io/v2/assets/426526699',
           function(err, data3) {    
-            thingie3 = data3.asset['asset-tx-counter'];
             getJSON('https://indexer.algoexplorerapi.io/v2/assets/393495312',
           function(err, data4) {    
-            thingie4 = data4.asset['asset-tx-counter'];
             getJSON('https://indexer.algoexplorerapi.io/v2/assets/305992851',
           function(err, data5) {    
-            thingie5 = data5.asset['asset-tx-counter'];
 
     var jsObj = [
-        {"Asdf":"1", "Unit_Name": "ACORN", "ID": "226265212", "Algo Verified": "TBD", "Name": "AlgoNuts", "TX_Counter": thingie1, "Work History": "TBD",
+        {"Asdf":"1", "Unit_Name": "ACORN", "ID": "226265212", "Algo Verified": "TBD", "Name": "AlgoNuts", "TX_Counter": data.asset['asset-tx-counter'], "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Discord, Email, TinyChart, Twitter"},
-        {"Asdf":"2", "Unit_Name": "ACRES", "ID": "555540865", "Algo Verified": "TBD", "Name": "80Acres", "TX_Counter": thingie2, "Work History": "TBD",
+        {"Asdf":"2", "Unit_Name": "ACRES", "ID": "555540865", "Algo Verified": "TBD", "Name": "80Acres", "TX_Counter": data2.asset['asset-tx-counter'], "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Email, Phone, TinyChart"},
-        {"Asdf":"3", "Unit_Name": "ADVENTUR", "ID": "426526699", "Algo Verified": "TBD", "Name": "Adventure Coin", "TX_Counter": thingie3, "Work History": "TBD",
+        {"Asdf":"3", "Unit_Name": "ADVENTUR", "ID": "426526699", "Algo Verified": "TBD", "Name": "Adventure Coin", "TX_Counter": data3.asset['asset-tx-counter'], "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Reddit, TinyChart, Twitter"},
-        {"Asdf":"4", "Unit_Name": "AFD", "ID": "393495312", "Algo Verified": "TBD", "Name": "Algorand Faucet Drops", "TX_Counter": thingie4, "Work History": "TBD",
+        {"Asdf":"4", "Unit_Name": "AFD", "ID": "393495312", "Algo Verified": "TBD", "Name": "Algorand Faucet Drops", "TX_Counter": data4.asset['asset-tx-counter'], "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "Yes", "Roadmap": "TBD", "Wallets": "Creator, Reserve", "Other Links": "Discord, TinyChart, Twitter"},
-        {"Asdf":"5", "Unit_Name": "AGNR", "ID": "305992851", "Algo Verified": "TBD", "Name": "Algoneer", "TX_Counter": thingie5, "Work History": "Team (1,2)",
+        {"Asdf":"5", "Unit_Name": "AGNR", "ID": "305992851", "Algo Verified": "TBD", "Name": "Algoneer", "TX_Counter": data5.asset['asset-tx-counter'], "Work History": "Team (1,2)",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Discord, Ecosystem, GitHub, Reddit, TinyChart"}];            
-     
             
     var exampleTable = $('#example')
         .DataTable({
@@ -83,8 +71,6 @@ $(document)
                 { 'data': 'Wallets' },
                 { 'data': 'Other Links' },
             ]
-        });        
+        }); 
 
-    });  
-
-    });    }); }); }); }); 
+    }); }); }); }); }); }); 
