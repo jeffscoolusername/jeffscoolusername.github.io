5f180ff12e70c6134610c89494650128cf332ba4
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-14 21:14:43 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -17,29 +17,40 @@ $(document)
     };
 
     var thingie1 = '';   
-    var thingie2 = '226265212';
+    var thingie2 = '';
     var thingie3 = '';
     var thingie4 = '';
     var thingie5 = '';   
 
-    getJSON('https://indexer.algoexplorerapi.io/v2/assets/'[thingie2],
-          function(err, data) {      
-            
+    getJSON('https://indexer.algoexplorerapi.io/v2/assets/226265212',
+          function(err, data) {                  
             thingie1 = data.asset['asset-tx-counter'];
+            getJSON('https://indexer.algoexplorerapi.io/v2/assets/555540865',
+          function(err, data2) {    
+            thingie2 = data2.asset['asset-tx-counter'];
+            getJSON('https://indexer.algoexplorerapi.io/v2/assets/426526699',
+          function(err, data3) {    
+            thingie3 = data3.asset['asset-tx-counter'];
+            getJSON('https://indexer.algoexplorerapi.io/v2/assets/393495312',
+          function(err, data4) {    
+            thingie4 = data4.asset['asset-tx-counter'];
+            getJSON('https://indexer.algoexplorerapi.io/v2/assets/305992851',
+          function(err, data5) {    
+            thingie5 = data5.asset['asset-tx-counter'];
 
     var jsObj = [
         {"Asdf":"1", "Unit_Name": "ACORN", "ID": "226265212", "Algo Verified": "TBD", "Name": "AlgoNuts", "TX_Counter": thingie1, "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Discord, Email, TinyChart, Twitter"},
-        {"Asdf":"2", "Unit_Name": "ACRES", "ID": "555540865", "Algo Verified": "TBD", "Name": "80Acres", "TX_Counter": "90000", "Work History": "TBD",
+        {"Asdf":"2", "Unit_Name": "ACRES", "ID": "555540865", "Algo Verified": "TBD", "Name": "80Acres", "TX_Counter": thingie2, "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Email, Phone, TinyChart"},
-        {"Asdf":"3", "Unit_Name": "ADVENTUR", "ID": "426526699", "Algo Verified": "TBD", "Name": "Adventure Coin", "TX_Counter": "55", "Work History": "TBD",
+        {"Asdf":"3", "Unit_Name": "ADVENTUR", "ID": "426526699", "Algo Verified": "TBD", "Name": "Adventure Coin", "TX_Counter": thingie3, "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Reddit, TinyChart, Twitter"},
-        {"Asdf":"4", "Unit_Name": "AFD", "ID": "393495312", "Algo Verified": "TBD", "Name": "Algorand Faucet Drops", "TX_Counter": "44", "Work History": "TBD",
+        {"Asdf":"4", "Unit_Name": "AFD", "ID": "393495312", "Algo Verified": "TBD", "Name": "Algorand Faucet Drops", "TX_Counter": thingie4, "Work History": "TBD",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "Yes", "Roadmap": "TBD", "Wallets": "Creator, Reserve", "Other Links": "Discord, TinyChart, Twitter"},
-        {"Asdf":"5", "Unit_Name": "AGNR", "ID": "305992851", "Algo Verified": "TBD", "Name": "Algoneer", "TX_Counter": "39", "Work History": "Team (1,2)",
+        {"Asdf":"5", "Unit_Name": "AGNR", "ID": "305992851", "Algo Verified": "TBD", "Name": "Algoneer", "TX_Counter": thingie5, "Work History": "Team (1,2)",
         "Algo Grant": "TBD", "Company": "TBD", "Paper": "TBD", "Roadmap": "TBD", "Wallets": "Creator, Manager, Reserve", 
         "Other Links": "Discord, Ecosystem, GitHub, Reddit, TinyChart"}];            
      
@@ -76,14 +87,4 @@ $(document)
 
     });  
 
-    //alert("New Username: " + jsObj[1].TX_Counter);
-
-    /*var myTable = document.getElementById('example');
-    myTable.rows[1].cells[4].innerHTML = thingie1;
-    myTable.rows[2].cells[4].innerHTML = thingie2;
-    myTable.rows[3].cells[4].innerHTML = thingie3;
-    myTable.rows[4].cells[4].innerHTML = thingie4;
-    myTable.rows[5].cells[4].innerHTML = thingie5;*/
-
-
-    });  
+    });    }); }); }); }); 
