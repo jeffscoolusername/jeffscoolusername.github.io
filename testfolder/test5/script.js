41ec85ef2d06772118da2ad1682daeea066834d0
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-16 18:46:41 -0700

    Update script.js

--- testfolder/test5/script.js
+++ testfolder/test5/script.js
@@ -36,17 +36,6 @@ $(document)
     var p13 = 'loading';
     var p14 = 'loading';
     
-    var myTable = document.getElementById('example');
-
-    var jsObj = [
-        {'Element':'0', 'Unit Name': p0, 'ID': p1, 'Created at Round': p2,      
-        'Name': p3, 'Transactions Counter': p4, 'Circulating Supply': p5,
-        'Total Supply': p6, '% in Circulation': p7 + '%'},
-        {'Element':'1', 'Unit Name': p7, 'ID': p8, 'Created at Round': p9,      
-        'Name': p10, 'Transactions Counter': p11, 'Circulating Supply': p12,
-        'Total Supply': p13, '% in Circulation': p14 + '%'}];     
-
-    
     getJSON('https://indexer.algoexplorerapi.io/v2/assets/226265212?include-all=true',
         function(err, data) {
             if (err !== null) {
