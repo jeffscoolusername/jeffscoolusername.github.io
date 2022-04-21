01227afc19b15dbd14f7fe79ee57e2288bb12d3d
jeffscoolusername <jeffscoolemail@gmail.com>
2022-04-20 21:24:07 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -16,7 +16,27 @@ $(document)
             xhr.send();
         };
 
-        //window.alert = function() {};
+        jQuery.event.special.touchstart = {
+            setup: function( _, ns, handle ) {
+                this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+            }
+        };
+        jQuery.event.special.touchmove = {
+            setup: function( _, ns, handle ) {
+                this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+            }
+        };
+        jQuery.event.special.wheel = {
+            setup: function( _, ns, handle ){
+                this.addEventListener("wheel", handle, { passive: true });
+            }
+        };
+        jQuery.event.special.mousewheel = {
+            setup: function( _, ns, handle ){
+                this.addEventListener("mousewheel", handle, { passive: true });
+            }
+        };
+        window.alert = function() {};
 
         var algoexplorerapi = 'https://indexer.algoexplorerapi.io/v2/assets/';
         var algochartsapi = 'https://algocharts.net/apiv2/?asset_in=';
