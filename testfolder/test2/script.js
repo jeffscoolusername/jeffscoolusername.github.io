3f211736696fd4638152a3591dc01abc407fedc6
jeffscoolusername <jeffscoolemail@gmail.com>
2022-05-03 16:46:50 -0700

    Update script.js

--- testfolder/test2/script.js
+++ testfolder/test2/script.js
@@ -2918,18 +2918,7 @@ $(document)
                             }
                         },
                         {
-                           "targets": [4],
-                            "render": function ( data, type, row, meta ) {
-                                //var linkedIn = row[0];                   
-                                //return '<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/' + itemID + '">' + itemID + '</a>';
-                                if (data === "TBD")
-                                {return data;}
-                                else
-                                {return '<a target="_blank" rel="noopener noreferrer" href="' + data + '">' + 'Yes' + '</a>';}
-                            }
-                        },
-                        {
-                           "targets": [7],
+                           "targets": [4, 7],
                             "render": function ( data, type, row, meta ) {
                                 //var linkedIn = row[0];                   
                                 //return '<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/' + itemID + '">' + itemID + '</a>';
