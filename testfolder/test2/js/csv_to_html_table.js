39a32facd94c1afa82970fbc14fbc47c64602ab8
jeffscoolusername <jeffscoolemail@gmail.com>
2022-05-06 19:21:05 -0700

    Update csv_to_html_table.js

--- testfolder/test2/js/csv_to_html_table.js
+++ testfolder/test2/js/csv_to_html_table.js
@@ -16,7 +16,7 @@ CsvToHtmlTable = {
             customTemplates[colIdx] = func;
         });
 
-        var $table = $("<table class='table table-striped table-condensed' id='" + el + "-table'></table>"); //causing issues somehow...
+        var $table = $("<table role='table' class='table table-striped table-bordered hover display nowrap' id='" + el + "-table'></table>"); //causing issues somehow...
         var $containerElement = $("#" + el);
         $containerElement.empty().append($table);
 
